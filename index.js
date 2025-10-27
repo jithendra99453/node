5feb44d61198cb173be36303d3d533b96444c246
jithendra Dusanapudi <jithendradusanapudi99@gmail.com>
2025-10-27 13:40:22 +0530

    learned node modules and event emitter

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
-const {format}=require('date-fns');
-const { v4: uuid }=require('uuid');
-console.log(format(new Date(), 'yyyyMMdd\tHH:mm:ss'));
-console.log(uuid());
+const {logEvents}=require('./logEvents');
+const EventEmitter=require('events');
+
+class MyEmitter extends EventEmitter{ };
+
+const myEmitter=new MyEmitter();
+
+myEmitter.on('log',(msg)=>logEvents(msg));
+setTimeout(()=>{
+    myEmitter.emit('log','log event emitted!');
+},2000);
